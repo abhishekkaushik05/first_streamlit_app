78e13062c5292cdaeb460d0db7ae44eca3d2a59e
abhishekkaushik05 <103560800+abhishekkaushik05@users.noreply.github.com>
2023-01-30 18:33:39 +0530

    Update streamlit_app.py
    load fruits file from s3 bucket using pandas

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,5 @@
 import streamlit as st
-
+import pandas as pd
 st.title('My Parents New Healthy Diner')
 
 st.header('Breakfast Favorites')
@@ -8,4 +8,7 @@ st.text('🥗 Kale, Spinach and Rocket Smoothie')
 st.text('🐔 Hard-Boiled Free-Range Egg')
 st.text('🥑🍞 Avocado Toast')
 st.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+st.dataframe(my_fruit_list)
 
