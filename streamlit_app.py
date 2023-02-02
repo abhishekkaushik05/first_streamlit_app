7a1c7a8ff80a77c1d0cc6e9ef2afe177199746af
abhishekkaushik05 <103560800+abhishekkaushik05@users.noreply.github.com>
2023-02-02 15:38:34 +0530

    Update streamlit_app.py
    converted api response into json

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,4 +19,5 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 st.dataframe(fruits_to_show)
 
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-st.text(fruityvice_response)
+st.text(fruityvice_response.json())
+st.header("Fruityvice Fruit Advice!")
