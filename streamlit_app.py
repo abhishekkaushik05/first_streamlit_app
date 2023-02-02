fd388cbe1e40d8288ac59e00f8828d7405fbe72c
abhishekkaushik05 <103560800+abhishekkaushik05@users.noreply.github.com>
2023-02-02 16:02:38 +0530

    Update streamlit_app.py
    added text box to take input from user

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,3 +26,6 @@ st.header("Fruityvice Fruit Advice!")
 fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
 # display the tabular data on screen
 st.dataframe(fruityvice_normalized)
+
+fruit_choice = st.text_input('What fruit would you like information about?','Kiwi')
+st.write('The user entered ', fruit_choice)
