604f33033511deaa78184d89ff6f7166e428b258
abhishekkaushik05 <103560800+abhishekkaushik05@users.noreply.github.com>
2023-02-02 16:28:33 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -29,3 +29,4 @@ st.dataframe(fruityvice_normalized)
 
 fruit_choice = st.text_input('What fruit would you like information about?','Kiwi')
 st.write('The user entered ', fruit_choice)
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
